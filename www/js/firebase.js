a3f23fbca145756604b702dc007f0c19c8797e78
Felipe256 <fcavalcante2013.fc@gmail.com>
2021-10-06 02:36:55 -0300

    CRUD working

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAAljQHadQtby4aPeI7aIjVVfUKFDoeAgg",
+	authDomain: "app-agendamentos-6d3dd.firebaseapp.com",
+	projectId: "app-agendamentos-6d3dd",
+	storageBucket: "app-agendamentos-6d3dd.appspot.com",
+	messagingSenderId: "850366465348",
+	appId: "1:850366465348:web:5e92248bd67262fddec311",
+	measurementId: "G-KCJCJHKY7L"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
